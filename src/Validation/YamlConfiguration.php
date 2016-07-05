a168baf58305a6ed6baec8a0516e3ce97cd606dc
Dave Meikle <david@quantumunit.com>
2016-07-05 13:05:54 -0700

    added configuration exception

--- src/Validation/YamlConfiguration.php
+++ src/Validation/YamlConfiguration.php
@@ -1,11 +1,10 @@
 <?php
 
-
 namespace Validation;
 
 use Validation\ConfigLoaderInterface;
 use Symfony\Component\Yaml\Yaml;
-
+use Validation\Exceptions\ConfigurationNotLoadedException;
 
 /**
  * Description of YamlConfiguration
@@ -13,23 +12,27 @@ use Symfony\Component\Yaml\Yaml;
  * @author davem
  */
 class YamlConfiguration implements ConfigLoaderInterface {
-    
+
     private $config = null;
-    
+
     public function getConfig() {
         return $this->config;
     }
 
     public function loadConfig($filepath) {
-        if(!file_exists($filepath)) {
+        if (!file_exists($filepath)) {
             return false;
         }
         $this->config = Yaml::parse(file_get_contents($filepath));
     }
-    
+
     public function getNode($key) {
-        if(array_key_exists($key, $this->config)) {
+        if (is_null($this->config)) {
+            throw new ConfigurationNotLoadedException();
+        }
+        if (array_key_exists($key, $this->config)) {
             return $this->config[$key];
         }
     }
+
 }
